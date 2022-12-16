3ed1520f4a8efc9f099bcbfdc8adc52762585756
wu <shihao.wuu@gmail.com>
2022-12-16 20:05:55 +0800

    feat(spawn)

--- os5/src/task/task.rs
+++ os5/src/task/task.rs
@@ -1,7 +1,7 @@
 //! Types related to task management & Functions for completely changing TCB
 
 use super::TaskContext;
-use super::{pid_alloc, KernelStack, PidHandle};
+use super::{pid_alloc, KernelStack, PidHandle, add_task};
 use crate::config::{TRAP_CONTEXT, MAX_SYSCALL_NUM};
 use crate::mm::{MemorySet, PhysPageNum, VirtAddr, KERNEL_SPACE, MapPermission};
 use crate::sync::UPSafeCell;
@@ -135,6 +135,60 @@ impl TaskControlBlock {
             trap_handler as usize,
         );
         task_control_block
+    }
+    /// return child process's pid (or -1 if failed)
+    pub fn spawn(self: &Arc<TaskControlBlock>, elf_data: &[u8]) -> isize {
+        // ---- access parent PCB exclusively
+        let mut parent_inner = self.inner_exclusive_access();
+        // memory_set with elf program headers/trampoline/trap context/user stack
+        let (memory_set, user_sp, entry_point) = MemorySet::from_elf(elf_data);
+        let trap_cx_ppn = memory_set
+            .translate(VirtAddr::from(TRAP_CONTEXT).into())
+            .unwrap()
+            .ppn();
+        // alloc a pid and a kernel stack in kernel space
+        let pid_handle = pid_alloc();
+        let kernel_stack = KernelStack::new(&pid_handle);
+        let kernel_stack_top = kernel_stack.get_top();
+        let new_task = Arc::new(TaskControlBlock {
+            pid: pid_handle,
+            kernel_stack,
+            inner: unsafe {
+                UPSafeCell::new(TaskControlBlockInner {
+                    trap_cx_ppn,
+                    base_size: parent_inner.base_size,
+                    task_cx: TaskContext::goto_trap_return(kernel_stack_top),
+                    task_status: TaskStatus::Ready,
+                    memory_set,
+                    parent: Some(Arc::downgrade(self)),
+                    children: Vec::new(),
+                    exit_code: 0,
+                    syscall_times: vec![0; MAX_SYSCALL_NUM],
+                    first_scheduled: true,
+                    start_time: usize::MAX,
+                })
+            },
+        });
+        // add child
+        parent_inner.children.push(new_task.clone());
+        // modify kernel_sp in trap_cx
+        // **** access children PCB exclusively
+        // initialize trap_cx
+        let trap_cx = new_task.inner_exclusive_access().get_trap_cx();
+        *trap_cx = TrapContext::app_init_context(
+            entry_point,
+            user_sp,
+            KERNEL_SPACE.exclusive_access().token(),
+            kernel_stack_top,
+            trap_handler as usize,
+        );
+        // add new_task to schedular
+        let pid = new_task.getpid();
+        add_task(new_task);
+        // return
+        pid as isize
+        // ---- release parent PCB automatically
+        // **** release children PCB automatically
     }
     /// Load a new elf to replace the original application address space and start execution
     pub fn exec(&self, elf_data: &[u8]) {
